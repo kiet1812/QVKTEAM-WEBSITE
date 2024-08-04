bdd2243c30340ea723e872626b7d43b7d4b670fc
Pham Tuan Kiet <phamkiet1812@gmail.com>
2024-08-04 22:44:37 +0700

    add folder

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBar = document.querySelector('.menu_bar');
+    const navbar = document.querySelector('.navbar');
+    const helloworld = document.querySelector('.helloworld')
+
+    // Tạo thẻ <style> và thêm quy tắc CSS vào đó
+
+    menuBar.addEventListener('click', function() {
+        // Toggle class 'expanded' on navbar
+        navbar.classList.toggle('expanded');
+        helloworld.classList.toggle('Margin');
+        const style = document.createElement('style');
+        style.textContent = `
+            .button{
+                display: block;
+                bottom: 30px;
+                position: none;
+            }
+        `;
+        document.head.appendChild(style);
+    });
+});
+
+feather.replace()
